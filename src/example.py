572ab503c308d0e1a971da6b418b94a7928b9222
Viktor Claesson <claesson.viktor@gmail.com>
2021-12-08 17:45:04 +0100

    remove specific numbers from example

--- src/example.py
+++ src/example.py
@@ -1,11 +1,11 @@
 def part_one() -> int:
-    with open("aoc/4.txt") as file:
+    with open("aoc/XY.txt") as file:
         pass
     return 0
 
 
 def part_two() -> int:
-    with open("aoc/4.txt") as file:
+    with open("aoc/XY.txt") as file:
         pass
     return 0
 
