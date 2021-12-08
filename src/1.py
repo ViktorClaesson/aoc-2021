47647f51401d176b3db0e02894e0e7527cb3237c
Viktor Claesson <claesson.viktor@gmail.com>
2021-12-08 08:48:18 +0100

    change to numbers instead of lettered numbers as names

--- src/1.py
+++ src/1.py
@@ -1,11 +1,11 @@
 def part_one() -> int:
-    with open('aoc/one.txt') as file:
+    with open('aoc/1.txt') as file:
         ls = list(map(int, file.read().splitlines()))
         return sum(1 for c, n in zip(ls, ls[1:]) if c < n)
 
 
 def part_two() -> int:
-    with open('aoc/one.txt') as file:
+    with open('aoc/1.txt') as file:
         ls = list(map(int, file.read().splitlines()))
         ls = list(map(sum, zip(ls, ls[1:], ls[2:])))
         return sum(1 for c, n in zip(ls, ls[1:]) if c < n)
