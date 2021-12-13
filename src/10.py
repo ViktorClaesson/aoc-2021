247dee8781170da0ef4dd8d19b8234f5531280f3
Viktor Claesson <claesson.viktor@gmail.com>
2021-12-13 08:41:15 +0100

    day 10

---
+++ src/10.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+import re
+
+
+class Chunk:
+    # basically '()|[]|{}|<>' but escaped with '\' for special regex characters
+    regex_empty: re.Pattern = re.compile(r"\(\)|\[]|{}|<>")
+
+    # basically '(]|(}|(>|[)|[}|[>|{)|{]|{>|<)|<]|<}' but escaped with '\' for special regex characters
+    regex_corrupted: re.Pattern = re.compile(r"\(]|\(}|\(>|\[\)|\[}|\[>|{\)|{]|{>|<\)|<]|<}")
+
+    opener_to_closer: dict[str, str] = {"(": ")", "[": "]", "{": "}", "<": ">"}
+    corrupted_character_value: dict[str, int] = {")": 3, "]": 57, "}": 1197, ">": 25137}
+    completion_character_value: dict[str, int] = {")": 1, "]": 2, "}": 3, ">": 4}
+
+
+def get_corrupted_character(line: str) -> str | None:
+    while line:
+        if match := Chunk.regex_corrupted.search(line):
+            # Line is corrupted: return the corrupted character of the first match
+            return match.group()[1]
+        if line == (new_line := Chunk.regex_empty.sub("", line)):
+            # Line is incomplete: nothing to do
+            return None
+        line = new_line
+    # Line is completely valid: nothing to do
+    return None
+
+
+def part_one() -> int:
+    with open("src/10.txt") as file:
+        ls = file.read().splitlines()
+    return sum(Chunk.corrupted_character_value.get(corrupted_character) for line in ls if (corrupted_character := get_corrupted_character(line)))
+
+
+def get_completion_string(line: str) -> str | None:
+    while line:
+        if Chunk.regex_corrupted.search(line):
+            # Line is corrupted: nothing to do
+            return None
+        if line == (new_line := Chunk.regex_empty.sub("", line)):
+            # Line is incomplete: return ending that would complete it
+            return "".join(Chunk.opener_to_closer.get(ch) for ch in reversed(line))
+        line = new_line
+    # Line is completely valid: nothing to do
+    return None
+
+
+def get_completion_string_score(completion_string: str) -> int:
+    # abcd => ((a * 5 + b) * 5 + c) * 5 + d => 5**3 * a + 5**2 * b + 5**1 * c + 5**0 * d
+    return sum(5 ** (len(completion_string) - idx) * Chunk.completion_character_value.get(ch) for idx, ch in enumerate(completion_string, start=1))
+
+
+def part_two() -> int:
+    with open("src/10.txt") as file:
+        ls = file.read().splitlines()
+    scores: list[int] = sorted(get_completion_string_score(completion_string) for line in ls if (completion_string := get_completion_string(line)))
+    return scores[len(scores) // 2] if scores else 0
+
+
+if __name__ == "__main__":
+    print(part_one())
+    print(part_two())
